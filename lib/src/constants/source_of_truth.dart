daf08d999b0c0f888c77de1cf0ac78601a11a7b8
Kweayon  Clark <Kweayon@gmail.com>
2022-11-11 17:10:08 -0600

    Added Custom SizedBox

--- lib/src/constants/source_of_truth.dart
+++ lib/src/constants/source_of_truth.dart
@@ -7,3 +7,6 @@ const gaph24 = SizedBox(
 const gaph12 = SizedBox(
   height: 12.0,
 );
+
+// This SizedBox is used to control all of the Field heights in create_user_screen.dart
+Widget customSizedBox({required child}) => SizedBox(height: 60, child: child);
