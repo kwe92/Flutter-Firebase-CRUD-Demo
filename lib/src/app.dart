1c6728f6f6645be2f1dbcc4a8679a638224bb250
Kweayon  Clark <Kweayon@gmail.com>
2022-11-10 18:23:27 -0600

    Added Comments

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,8 @@
 import 'package:firebasedemo/src/features/create_user/presentation/create_user_screen.dart';
 import 'package:flutter/material.dart';
+//TODO: Add button to user page and decouple code a bit
+//TODO: push a user to Firebase
+//TODO: Create a base user class
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
