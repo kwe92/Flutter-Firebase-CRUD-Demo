79202ca1b3584ef11342cc1c08f7e9964b2e6daa
Kweayon  Clark <Kweayon@gmail.com>
2022-11-10 18:21:44 -0600

    Ipdate

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,14 @@
+import 'package:firebasedemo/src/features/create_user/presentation/create_user_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  static const _title = 'Firebase Demo';
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: null,
+      home: UserScreen(title: _title),
     );
   }
 }
