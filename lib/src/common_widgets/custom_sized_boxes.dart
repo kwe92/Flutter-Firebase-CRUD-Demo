2c00eb1088bae723128dc02c27c06db7eb3a1865
Kweayon  Clark <Kweayon@gmail.com>
2022-11-16 16:28:24 -0600

    Inital Commit
    Added a custom sized box file

---
+++ lib/src/common_widgets/custom_sized_boxes.dart
@@ -0,0 +1,7 @@
+// This SizedBox is used to control all of the Field heights in create_user_screen.dart
+import 'package:flutter/material.dart';
+
+Widget customSizedBox({required child}) => SizedBox(height: 60, child: child);
+
+Widget customSizedBoxButtonBox({required child}) =>
+    SizedBox(height: 38.0, child: child);
