e936d8c7f403912ba5770e58096f4f072b105c62
Kweayon  Clark <Kweayon@gmail.com>
2022-11-16 16:32:44 -0600

    Updates

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,10 @@
 import 'package:firebasedemo/src/features/create_user/presentation/create_user_screen.dart';
+import 'package:firebasedemo/src/features/update_delete_user/presentation/update_delete_user_screen.dart';
 import 'package:firebasedemo/src/features/view_users/presentation/current_users_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  static const _title = 'Firebase Demo';
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: <String, WidgetBuilder>{
         '/': (context) => const CurrentUserScreen(),
-        '/createuser': (context) => const UserScreen(title: _title)
+        '/createuser': (context) => const CreateUserScreen(),
+        '/update_delete_screen': (context) => const UpdateDeleteScreen()
       },
     );
   }
