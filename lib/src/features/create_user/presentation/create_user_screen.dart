b40cd3824959ab62ed0f7c9316c16f8feac806e4
Kweayon  Clark <Kweayon@gmail.com>
2022-11-16 16:30:08 -0600

    Refactor
    Refactored the buttons and cleaned up some duplicate code

--- lib/src/features/create_user/presentation/create_user_screen.dart
+++ lib/src/features/create_user/presentation/create_user_screen.dart
@@ -1,18 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_field/date_field.dart';
+import 'package:firebasedemo/src/common_widgets/custom_sized_boxes.dart';
 import 'package:firebasedemo/src/constants/source_of_truth.dart';
 import 'package:firebasedemo/src/features/create_user/domain/user.dart';
 import 'package:firebasedemo/src/common_widgets/custom_text_field.dart';
 import 'package:firebasedemo/src/common_widgets/main_scaffold.dart';
-import 'package:firebasedemo/src/common_widgets/text_field_decoration.dart';
 import 'package:flutter/material.dart';
 
-class UserScreen extends StatefulWidget {
-  const UserScreen({super.key, required this.title});
-  final String title;
+class CreateUserScreen extends StatefulWidget {
+  const CreateUserScreen({super.key});
 
   @override
-  State<UserScreen> createState() => _UserScreenState();
+  State<CreateUserScreen> createState() => _CreateUserScreenState();
 }
 
 //TODO: Move  _createUser logic to a repository and a data folder | user Riverpod architecture
@@ -25,7 +24,7 @@ Future<void> _createUser(
   await docUsers.set(json);
 }
 
-class _UserScreenState extends State<UserScreen> {
+class _CreateUserScreenState extends State<CreateUserScreen> {
   final nameController = TextEditingController();
   final ageController = TextEditingController();
   DateTime? selectedDate;
@@ -33,20 +32,20 @@ class _UserScreenState extends State<UserScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScaffold(
-      title: widget.title,
+      title: 'Firestore Demo: Create User',
       body: Center(
         child: ListView(
           //TODO: move EdgeInsets to source of truth file
-          padding: const EdgeInsets.all(24.0),
+          padding: CustomEdgeInsets.all24,
           children: <Widget>[
             customTextField(
               controller: nameController,
-              decoration: decoration(label: 'Name'),
+              decoration: FieldDecoration.decoration(label: 'Name'),
             ),
             gaph24,
             customTextField(
                 controller: ageController,
-                decoration: decoration(label: 'Age'),
+                decoration: FieldDecoration.decoration(label: 'Age'),
                 keyboardType: TextInputType.number),
             gaph24,
             customSizedBox(
@@ -59,14 +58,11 @@ class _UserScreenState extends State<UserScreen> {
                     selectedDate = value;
                   });
                 },
-                decoration: decoration(
-                  label: 'Birthday',
-                ),
+                decoration: FieldDecoration.decoration(label: 'Birthday'),
               ),
             ),
             gaph12,
-            SizedBox(
-              height: 38.0,
+            customSizedBoxButtonBox(
               child: ElevatedButton(
                 onPressed: () {
                   if (nameController.text.isNotEmpty &&
