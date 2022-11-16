ca3059ff8d7a244adb2019a9eb7fd9b825c60301
Kweayon  Clark <Kweayon@gmail.com>
2022-11-16 16:29:31 -0600

    Update
    Added some additional classes for decorations and edge insets

--- lib/src/constants/source_of_truth.dart
+++ lib/src/constants/source_of_truth.dart
@@ -8,7 +8,22 @@ const gaph12 = SizedBox(
   height: 12.0,
 );
 
-const TextStyle nameListTileTextStyle = TextStyle(fontSize: 18.0);
+class CustomTextStyles {
+  const CustomTextStyles();
+  static const TextStyle nameListTileTextStyle = TextStyle(fontSize: 18.0);
+}
 
-// This SizedBox is used to control all of the Field heights in create_user_screen.dart
-Widget customSizedBox({required child}) => SizedBox(height: 60, child: child);
+// Function for text field decorations
+class FieldDecoration {
+  static InputDecoration decoration({required String label}) => InputDecoration(
+        label: Text(label),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      );
+}
+
+class CustomEdgeInsets {
+  static const EdgeInsets all24 = EdgeInsets.all(24.0);
+  static const EdgeInsets top12 = EdgeInsets.only(top: 12.0);
+}
